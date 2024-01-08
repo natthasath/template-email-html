8ccc6112cb4b1ecc38ac09d0b6b86046040396ff
natthasath <natthasath.sak@gmail.com>
2024-01-08 13:47:51 +0700

    First Commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import resend
+from decouple import config
+
+resend.api_key = config("RESEND_KEY")
+
+file_path = 'template/confirm-email.html'
+with open(file_path, 'r', encoding='utf-8') as file:
+    file_content = file.read()
+
+r = resend.Emails.send({
+  "from": config("EMAIL_FROM"),
+  "to": config("EMAIL_TO"),
+  "subject": "Confirm Email",
+  "html": file_content
+})
